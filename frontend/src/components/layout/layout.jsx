76a03dde89ca3143f626a2fef0e5aa890eb5cc77
Azzerith <zikazabat@gmail.com>
2026-04-30 14:14:17 +0700

    feat: add main application layout component with sidebar and navbar integration

--- frontend/src/components/layout/layout.jsx
+++ frontend/src/components/layout/layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from './Navbar';
-import Sidebar from './Sidebar';
+import Navbar from './navbar';
+import Sidebar from './sidebar';
 
 function Layout() {
   return (
